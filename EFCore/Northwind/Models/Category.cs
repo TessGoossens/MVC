de89c3933c839d8ac2f661f6509ecaea5527720d
Tess Goossens <70571136+TessGoossens@users.noreply.github.com>
2021-11-11 11:41:31 +0100

    Northwind DataModel

---
+++ EFCore/Northwind/Models/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind.Models
+{
+    class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+        
+        [Key]
+        public int CategoryID { get; set; }
+
+        [Required, StringLength(15)]
+        public string CategoryName { get; set; }
+
+        [Column(TypeName = "ntext")]
+        public string Description { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+    
+
+}
---
+++ EFCore/Northwind/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind.Models
+{
+    class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required, StringLength(15)]
+        public string ProductName { get; set; }
+
+        [Required, StringLength(15)]
+        public string ProductSuppliers { get; set; }
+
+        [Required, StringLength(15)]
+        public string ProductsInStorrage { get; set; }
+    }   
+}
