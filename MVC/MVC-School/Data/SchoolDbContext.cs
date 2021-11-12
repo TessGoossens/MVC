bee61533204f31c0d8f0f0681312576f8aeeda0f
Tess Goossens <70571136+TessGoossens@users.noreply.github.com>
2021-11-12 10:41:44 +0100

    MVC School

--- MVC/MVC-School/Data/SchoolDbContext.cs
+++ MVC/MVC-School/Data/SchoolDbContext.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_School.Models;
 
 namespace MVC_School.Data
 {
     public class SchoolDbContext : DbContext
     {
+        public DbSet<Student> Studenten { get; set; }
 
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options) : base(options)
+        {
+        }
+        protected override void  OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
--- MVC/MVC-School/Models/Student.cs
+++ MVC/MVC-School/Models/Student.cs
@@ -2,22 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations
+using System.ComponentModel.DataAnnotations;
 
 namespace MVC_School.Models
 {
     public class Student
     {
-        [Key]
         public int ID { get; set; }
 
-        [Required]
         [MaxLength(20)]
         public string Voornaam { get; set; }
+
         [MaxLength(40)]
         public string Achternaam { get; set; }
+
         [MaxLength(40)]
         public string Adres { get; set; }
+
         [MaxLength(40)]
         public string Woonplaats { get; set; }
         
